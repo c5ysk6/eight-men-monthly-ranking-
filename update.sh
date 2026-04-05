a374e213aa4edb787e1740565d12fed80d5241c7
c5ysk6 <c5ysk6@users.noreply.github.com>
2026-04-05 13:52:53 +0900

    update.sh のパスを新しいディレクトリ構成に更新

--- update.sh
+++ update.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # EIGHT MEN ランキング 毎日自動更新スクリプト
 
-LOG="/Users/c5ysk6/Documents/eight-men-monthly-ranking/update.log"
+LOG="/Users/c5ysk6/Dropbox (個人)/GM/00_開発・自動化/売上ダッシュボード/スタッフランキング/eight-men-monthly-ranking/update.log"
 echo "===== $(date '+%Y-%m-%d %H:%M:%S') 開始 =====" >> "$LOG"
 
 # HTMLを生成
-python3 "/Users/c5ysk6/Dropbox (個人)/01_売上管理/売上ダッシュボード/generate_ranking.py" >> "$LOG" 2>&1
+python3 "/Users/c5ysk6/Dropbox (個人)/GM/00_開発・自動化/売上ダッシュボード/スタッフランキング/generate_ranking.py" >> "$LOG" 2>&1
 
 # GitHubにpush
-cd /Users/c5ysk6/Documents/eight-men-monthly-ranking
+cd /Users/c5ysk6/Dropbox (個人)/GM/00_開発・自動化/売上ダッシュボード/スタッフランキング/eight-men-monthly-ranking
 git add index.html >> "$LOG" 2>&1
 git commit -m "update $(date '+%Y-%m-%d')" >> "$LOG" 2>&1
 git push >> "$LOG" 2>&1
